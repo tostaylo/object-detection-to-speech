ccb94b2601f453396c1926e7ddfa22867e459bab
dhwcode <dhwcode@users.noreply.github.com>
2022-11-27 16:21:28 -0500

    fixed model_switch

--- webapp.py
+++ webapp.py
@@ -47,7 +47,7 @@ def predict_from_webcam():
   decoded = decode_base64_img(request.get_json())
   img = get_img_from_decoded(decoded)
 
-  if model_switch == "yolo":
+  if request.args.get('inference') == "yolo":
     prediction_name, confidence = get_yolo_predictions(yolo_model, img, 0)
   else:
     prediction_name, confidence = get_detectron_prediction(detectron_predictor, img, categories)
